587e4b4f9d124f515df4176b6753bcbb68a354f3
Sergey <serg78kh@yandex.ru>
2020-04-26 19:04:36 +0300

    fix bug in bot

--- data/tg_bot.py
+++ data/tg_bot.py
@@ -7,15 +7,15 @@ TOKEN = "1213926896:AAGeW9NZeh6AJebSQCyudrMcy10hQdfuH0E"
 
 def start(update, context):
     reply_keyboard = [["Написать отзыв", "Написать в техническую поддержку"]]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-    update.message.reply_text("В этом боте вы можете написать нам свой отзыв о нашем сайте"
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    update.message.reply_text("Этому боту вы можете написать нам свой отзыв о нашем сайте"
                               " или обратиться в техническую поддержку", reply_markup=markup)
 
 
 def write_feedback(update, context):
     reply_keyboard = [["Написать отзыв", "Написать в техническую поддержку"]]
     markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
-    update.message.reply_text("Ваш отзыв обязательно будет прочитан нами!", reply_markup=markup)
+    update.message.reply_text("Вы можете написать любой отзыв о нас!", reply_markup=markup)
 
 
 def write_about_error(update, context):
